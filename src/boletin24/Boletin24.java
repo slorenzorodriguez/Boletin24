af956e887be37b759995965593e826c6e9558a9d
slorenzorodriguez <slorenzorodriguez@nautilus22.danielcastelao.org>
2019-04-02 11:35:58 +0200

    Terminado.

---
+++ src/boletin24/Boletin24.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin24;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author slorenzorodriguez
+ */
+public class Boletin24 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Bol24Paleta showWindow = new Bol24Paleta();
+        showWindow.setVisible(true);
+        
+        
+        
+//       Interfaces marco1 = new Interfaces();
+//       marco1.crearventana();
+//       
+    }
+    
+}
